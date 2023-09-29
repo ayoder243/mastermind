a42f431c47131a1454e0d47f63c16d266d813512
Aaron Yoder <ayoder243@gmail.com>
2023-09-29 09:07:28 -0500

    Refactor blocks into functions

--- mastermind.rb
+++ mastermind.rb
@@ -1,46 +1,62 @@
-possible_colors = ["green", "yellow", "blue", "orange", "purple", "red"]
-code = []
-4.times do
-  code.push(possible_colors[rand(6)])
+POSSIBLE_COLORS = ["green", "yellow", "blue", "orange", "purple", "red"]
+
+def generate_code()
+  code = []
+  4.times do
+    code.push(POSSIBLE_COLORS[rand(6)])
+  end
+  return code
 end
+
+def get_guess()
+  guess = []
+  print POSSIBLE_COLORS
+  puts
+  print "Position 1: "
+  guess.push(gets.chomp())
+  print "Position 2: "
+  guess.push(gets.chomp())
+  print "Position 3: "
+  guess.push(gets.chomp())
+  print "Position 4: "
+  guess.push(gets.chomp())
+  return guess
+end
+
+def get_feedback(code, guess)
+  feedback = {
+    black: 0,
+    white: 0,
+  }
+
+  if guess == code
+    return "you win"  
+  else
+    new_code = []
+    new_guess = []
+    for i in (0..3)
+      if code[i] == guess[i]
+        feedback[:black] += 1
+      else
+        new_code.push(code[i])
+        new_guess.push(guess[i])
+      end
+    end
+    for i in new_code
+      if new_guess.include?(i)
+        feedback[:white] += 1
+        new_guess.delete_at(new_guess.index(i))
+      end
+    end
+    return feedback
+  end
+end
+
 turn_counter = 0
-guess = []
-puts "green, yellow, blue, orange, purple, red"
-print "Position 1: "
-guess.push(gets.chomp())
-print "Position 2: "
-guess.push(gets.chomp())
-print "Position 3: "
-guess.push(gets.chomp())
-print "Position 4: "
-guess.push(gets.chomp())
+code = generate_code()
+guess = get_guess()
+feedback = get_feedback(code, guess)
 
-
-black = 0
-white = 0
-
-if guess == code
-  puts "you win"  
-else
-  new_code = []
-  new_guess = []
-  for i in (0..3)
-    if code[i] == guess[i]
-      black += 1
-    else
-      new_code.push(code[i])
-      new_guess.push(guess[i])
-    end
-  end
-  for i in new_code
-    if new_guess.include?(i)
-      white += 1
-      new_guess.delete_at(new_guess.index(i))
-    end
-  end
-  print new_code, new_guess
-end
 print code
 print guess
-
-puts black, white
+print feedback
